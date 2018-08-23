e5a57ce3cb45ec9f5ecd3d515a3f3e2095c33c3a
jai-007n <jai007nnn@gmail.com>
2018-08-23 21:08:18 +0530

    Redux init

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Router,Route,IndexRoute,hashHistory} = require('react-router');
+
+
+// require('style!css!foundation-sites/dist/css/foundation.min.css');
+require('style-loader!css-loader!foundation-sites/dist/css/foundation-float.min.css');
+require('style!css!sass!applicationStyles')
+$(document).foundation();
+
+ReactDOM.render(
+<h1>Learn Redux</h1>
+  , document.getElementById('app'));
+
+  require('./redux-example');
